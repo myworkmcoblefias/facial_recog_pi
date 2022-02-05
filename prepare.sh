357b841ee77603927da834d9d4b4fb429e461b7d
myworkmcoblefias <48803444+myworkmcoblefias@users.noreply.github.com>
2022-02-05 22:30:52 +0800

    Add files via upload

---
+++ prepare.sh
@@ -0,0 +1,24 @@
+# Update and Upgrade
+echo "Updating and upgrading OS"
+sudo apt-get update && sudo apt-get upgrade
+
+# Install Docker
+echo "Installing docker"
+curl -sSL https://get.docker.com | sh
+
+# Add a Non-Root User to the Docker Group
+echo "Adding a Non-Root User to the Docker Group"
+sudo usermod -aG docker pi
+sudo usermod -aG docker ${USER}
+groups ${USER}
+
+# Install Docker-Compose
+echo "Installing docker-compose"
+sudo apt-get install libffi-dev libssl-dev
+sudo apt install python3-dev
+sudo apt-get install -y python3 python3-pip
+sudo pip3 install docker-compose
+
+# Enable the Docker system service to start your containers on boot
+echo "Enable the Docker system service to start your containers on boot"
+sudo systemctl enable docker
